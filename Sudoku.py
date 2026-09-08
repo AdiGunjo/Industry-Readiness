de06a70d048bdf17e25a6203883fc68ac91dca72
Harkonnen <gunjotikaraditya@gmail.com>
2026-09-08 07:27:04 +0530

    Backtracking

---
+++ Sudoku.py
@@ -0,0 +1,48 @@
+def solve_sudoku(board):
+    empty = find_empty(board)
+    if not empty:
+        return True
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0  # backtrack
+
+    return False
+
+def find_empty(board):
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == 0:
+                return (r, c)
+    return None
+
+def is_valid(board, row, col, num):
+    if num in board[row]:
+        return False
+    if num in [board[r][col] for r in range(9)]:
+        return False
+    box_r, box_c = 3 * (row // 3), 3 * (col // 3)
+    for r in range(box_r, box_r + 3):
+        for c in range(box_c, box_c + 3):
+            if board[r][c] == num:
+                return False
+    return True
+
+puzzle = [
+    [5,3,0, 0,7,0, 0,0,0],
+    [6,0,0, 1,9,5, 0,0,0],
+    [0,9,8, 0,0,0, 0,6,0],
+    [8,0,0, 0,6,0, 0,0,3],
+    [4,0,0, 8,0,3, 0,0,1],
+    [7,0,0, 0,2,0, 0,6,0],
+    [0,6,0, 0,0,0, 2,8,0],
+    [0,0,0, 4,1,9, 0,0,5],
+    [0,0,0, 0,8,0, 0,7,9],
+]
+if solve_sudoku(puzzle):
+    for row in puzzle:
+        print(row)
